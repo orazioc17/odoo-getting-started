2086058573c6851e63333d7fb5d1d1dc09c8cf02
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2023-05-01 15:17:15 -0400

    Chapter 4: Models and Basic Fields

---
+++ custom/estate/models/estate_property.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+
+class EstateProperties(models.Model):
+    _name = 'estate.property'
+    _description = 'Real Estate Advertising properties table'
+    
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="Garden Orientation shows in which direction is the garden oriented")
