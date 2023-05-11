72cd58d89366e8ca9daf7b6ccaf9e0b44fc132ca
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2023-05-11 09:19:06 -0400

    Chapter 6: Finally, Some UI To Play With

--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -7,10 +7,10 @@ class EstateProperties(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Date.today())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,3 +20,11 @@ class EstateProperties(models.Model):
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Garden Orientation shows in which direction is the garden oriented")
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        required=True,
+        copy=False,
+        default='new'
+    )
